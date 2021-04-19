4178651dbd8ebd1a3e14f6499c7c446f2bee770e
Daniel Kim <kkdh1109@gmail.com>
2021-04-19 19:25:51 -0400

    create env variables, connect to MongoDB, and fetch movie images in the landing page

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_KEY = '71287c918903884f4e6499d74b8870f0';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_URL = 'http://image.tmbdb.org/t/p/';
