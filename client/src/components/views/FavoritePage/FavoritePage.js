78ed5a6346f74b50d5b42b8d6208f271c436520a
Daniel Kim <kkdh1109@gmail.com>
2021-04-23 17:17:21 -0400

    insert basic HTML template for favorite page

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -3,10 +3,25 @@ import './FavoritePage.scss';
 
 function FavoritePage() {
     return (
-        <div>
-            
+        <div className='fav'>
+            <h3>Favorite Movies By Me</h3>
+            <hr />
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Movie Title</th>
+                        <th>Movie RunTime</th>
+                        <th>Remove from favorites</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                </tbody>
+            </table>
         </div>
     )
 }
 
-export default FavoritePage
+export default FavoritePage;
